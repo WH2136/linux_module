9cd904f4f793a38f6cad68419ce6dc9e0eaaf9e5
Jesse Wang <wjq1060115265@126.com>
2016-08-14 12:00:20 +0800

    Update hello.c

--- src/helloworld_module/hello.c
+++ src/helloworld_module/hello.c
@@ -3,14 +3,19 @@
 
 MODULE_LICENSE("GPL");
 
+/* 当模块被加载时执行该函数 */
 static int hello_init(void) {
 	printk(KERN_ALERT "hello world!\n");
 	return 0;
 }
-
+/* 当模块被卸载时执行该函数 */
 static void hello_exit(void) {
 	printk(KERN_ALERT"Goodbye, beautiful world\n");
 }
 
+/*
+ * module_init指定模块被加载的时候需要执行的函数
+ * module_exit指定模块被卸载的时候需要执行的函数
+ */
 module_init(hello_init);
 module_exit(hello_exit);
